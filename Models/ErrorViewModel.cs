f1d9bca80aad9142d02ae7805951894241c44a8f
IGNACIO GUTMAN <46917360@ort>
2022-06-30 14:33:20 -0300

    Create .NET MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sala_de_Escape.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
